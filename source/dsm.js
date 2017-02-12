3e701f35b19712df0ec6b7b7355c936906743486
David Ash <green.neon@gmail.com>
2017-02-12 16:59:00 -0700

    Fix for issue 5: Per-status reducers (parent reducers) solution

--- source/dsm.js
+++ source/dsm.js
@@ -1,18 +1,20 @@
 const camelCase = require('lodash.camelcase');
 const snakeCase = require('lodash.snakecase');
 
+const defaultStatus = 'idle';
+
 const parseNode = node => {
   const data = node.slice(0, 2);
   const child = node.slice(2);
   return { data, child };
 };
 
-const getStates = (graph, initialMemo = []) => (
+const getStates = (graph, initialMemo = [], parentStatus = defaultStatus) => (
   graph.reduce((memo, node) => {
     const { data, child } = parseNode(node);
 
-    if (Array.isArray(data)) memo = memo.concat([data]);
-    if (Array.isArray(child)) memo = getStates(child, memo);
+    if (Array.isArray(data)) memo = memo.concat([data.concat([parentStatus])]);
+    if (Array.isArray(child)) memo = getStates(child, memo, data[1]);
 
     return memo;
   }, initialMemo)
@@ -25,14 +27,13 @@ const empty = {
 };
 
 const defaultState = {
-  status: 'idle',
+  status: defaultStatus,
   payload: empty
 };
 
-const createReducer = (defaultState, reducerMap) => {
+const createReducer = (defaultState, parentReducerMap) => {
   return (state = defaultState, action = {}) => {
-    const { type } = action;
-    const reducer = reducerMap[type];
+    const reducer = parentReducerMap[state.status];
 
     if (typeof reducer === 'function') {
       return reducer(state, action);
@@ -65,16 +66,28 @@ const dsm = ({
       formatConstant(a[0])
     ].join('');
     const status = a[1];
+    const parentStatus = a[2];
 
     return {
       action,
-      status
+      status,
+      parentStatus
     };
   });
   const actions = actionMap.map(a => a.action);
 
+  const parentStatuses = Object.keys(actionMap.reduce((map, a) => {
+    map[a.parentStatus] = true;
+    return map;
+  }, {}));
+
+  const initialReducerMap = parentStatuses.reduce((map, status) => {
+    map[status] = {};
+    return map;
+  }, {});
+
   const reducerMap = actionMap.reduce((map, a) => {
-    map[a.action] = (state = defaultState, action = {}) => {
+    map[a.parentStatus][a.action] = (state = defaultState, action = {}) => {
 
       if (action.type === a.action) {
         const { status } = a;
@@ -88,9 +101,24 @@ const dsm = ({
       return state;
     };
     return map;
+  }, initialReducerMap);
+
+  const parentReducerMap = parentStatuses.reduce((map, status) => {
+    map[status] = (state = defaultState, action = {}) => {
+
+      const { type } = action;
+      const reducer = reducerMap[status][type];
+
+      if (typeof reducer === 'function') {
+        return reducer(state, action);
+      }
+
+      return state;
+    };
+    return map;
   }, {});
 
-  const reducer = createReducer(defaultState, reducerMap);
+  const reducer = createReducer(defaultState, parentReducerMap);
   const actionCreatorNames = getActionCreatorNames(states);
   const actionCreators = actionCreatorNames.reduce((acs, description, i) => {
     acs[description] = payload => ({
