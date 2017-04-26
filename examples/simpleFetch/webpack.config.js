8e0528a951382713b2e880291eea04bd30456d60
Bogna Knychała <bknychala@egnyte.com>
2017-04-26 19:02:12 +0200

    add index.html

--- examples/simpleFetch/webpack.config.js
+++ examples/simpleFetch/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     devtool: 'source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        contentBase: path.join(__dirname, '/'),
         compress: true,
         port: 9000
     }
