30f77468f746b590c6cb7e2707ee61943d4775fe
Eric Elliott <support@paralleldrive.com>
2018-01-04 21:19:38 -0800

    bug: initialState is always "idle"

---
+++ source/test/authenticate-flow-test.js
@@ -0,0 +1,49 @@
+import { describe } from 'riteway';
+
+import dsm from '../dsm';
+
+const actionStates = [
+  ['initialize', 'signed out',
+    ['sign in', 'authenticating'
+      // ['report error', 'error',
+      //   ['handle error', 'signed out']
+      // ],
+      // ['sign-in success', 'signed in']
+    ]
+  ]
+];
+
+const { initialize, reducer, signIn } = dsm({
+  component: 'user-authentication',
+  description: 'authenticate user',
+  actionStates
+});
+
+
+const createState = ({
+  status = 'signed out',
+  payload: { type = 'empty'} = {}
+} = {}) => ({ status, payload: { type } });
+
+describe('userAuthenticationReducer', async should => {
+  {
+    const {assert} = should('return use "signed out" as initialized state');
+
+    assert({
+      given: '["initialize", "signed out", /*...*/',
+      actual: reducer(),
+      expected: createState()
+    });
+  }
+
+  {
+    const {assert} = should('transition into authenticating state');
+    const initialState = reducer(undefined, initialize());
+
+    assert({
+      given: 'signed out initial state & signIn action',
+      actual: reducer(initialState, signIn()),
+      expected: createState()
+    });
+  }
+});
--- tools/cli.js
+++ tools/cli.js
@@ -4,10 +4,11 @@ require('colors');
  * Checks node version and prints a message before tests start
  * Latest Nodejs or at least 6.x.x is required to run our ES6 tests
  * @param {String} nodeVersion node semantic version
+ * @returns {undefined}
  */
-function checkNode (nodeVersion) {
-  if (Number(nodeVersion.slice('.')[0]) < 6) {
-    console.log('Tests require Node version > v6.0.0 to run'.red);
+function checkNode(nodeVersion) {
+  if (Number(nodeVersion.slice('.')[0]) < 8) {
+    console.log('Tests require Node version > v8.0.0 to run'.red);
   }
 }
 
